f784ca7059439c6cd7799e3a3668251491bb9298
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-03-05 16:03:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,17 +23,10 @@ def monopoly_with_third_tier
         four_pieces_owned: 200
       },
       names: {
-        reading_railroad:
-      {
-        
-      },
-      pennsylvania_railroad:{},
-      b_and_o_railroad:
-      {
-      },
-      shortline: 
-      {
-      }
+        reading_railroad: {},
+        pennsylvania_railroad:{},
+        b_and_o_railroad: {},
+        shortline_railroad: {}
       }
     }
   }
